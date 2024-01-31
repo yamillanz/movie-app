b139cbda8b3490eae0296ab593ec503bc7151a62
Yamil Lanz <yamil.lanz@gmail.com>
2024-01-31 14:15:12 -0400

    add some test

--- src/app/movies/delete-dialog/delete-dialog.component.spec.ts
+++ src/app/movies/delete-dialog/delete-dialog.component.spec.ts
@@ -1,23 +1,51 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { DeleteDialogComponent } from './delete-dialog.component';
+import { MovieDTO } from '../models/movies';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 describe('DeleteDialogComponent', () => {
   let component: DeleteDialogComponent;
   let fixture: ComponentFixture<DeleteDialogComponent>;
+  let dialogRefSpy: jasmine.SpyObj<MatDialogRef<DeleteDialogComponent>>;
+  let dialogData: MovieDTO;
 
   beforeEach(async () => {
+    dialogRefSpy = jasmine.createSpyObj('MatDialogRef', ['close']);
+
     await TestBed.configureTestingModule({
-      imports: [DeleteDialogComponent]
-    })
-    .compileComponents();
-    
+      imports: [DeleteDialogComponent],
+      providers: [
+        { provide: MatDialogRef, useValue: dialogRefSpy },
+        { provide: MAT_DIALOG_DATA, useValue: dialogData },
+      ],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(DeleteDialogComponent);
     component = fixture.componentInstance;
+    component.data = {
+      id: '1',
+      primaryImage: '',
+      titleText: 'Title',
+      releaseYear: 2021,
+    } as MovieDTO;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should close dialog with delete:true and data when delete is called', () => {
+    dialogData = {
+      id: '1',
+      primaryImage: '',
+      titleText: 'Title',
+      releaseYear: 2021,
+    } as MovieDTO;
+    component.delete();
+    expect(dialogRefSpy.close).toHaveBeenCalledWith({
+      delete: true,
+      data: dialogData,
+    });
   });
 });
--- src/app/shared/global-message/global-message.component.spec.ts
+++ src/app/shared/global-message/global-message.component.spec.ts
@@ -1,6 +1,7 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { GlobalMessageComponent } from './global-message.component';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { GlobalMessageComponent } from "./global-message.component";
 
 describe('GlobalMessageComponent', () => {
   let component: GlobalMessageComponent;
@@ -11,7 +12,7 @@ describe('GlobalMessageComponent', () => {
       imports: [GlobalMessageComponent]
     })
     .compileComponents();
-    
+
     fixture = TestBed.createComponent(GlobalMessageComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -20,4 +21,19 @@ describe('GlobalMessageComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have MatSnackBarLabel', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('[matSnackBarLabel]')).toBeTruthy();
+  });
+
+  // it('should have MatSnackBarActions', () => {
+  //   const compiled = fixture.debugElement.nativeElement;
+  //   expect(compiled.querySelector('mat-snack-bar-actions')).toBeTruthy();
+  // });
+
+  // it('should have MatSnackBarAction', () => {
+  //   const compiled = fixture.debugElement.nativeElement;
+  //   expect(compiled.querySelector('mat-snack-bar-action')).toBeTruthy();
+  // });
 });
