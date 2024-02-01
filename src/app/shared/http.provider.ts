1b2bda38c89478414b6aaa848542197d160f3f0c
Yamil Lanz <yamil.lanz@gmail.com>
2024-02-01 08:28:42 -0400

    add interceptor and cleanning code

--- src/app/shared/http.provider.ts
+++ src/app/shared/http.provider.ts
@@ -1,10 +1,8 @@
 import { Provider } from '@angular/core';
 
-// Injection token for the Http Interceptors multi-provider
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RapidApiInterceptor } from './http.interceptor';
 
-/** Provider for the Noop Interceptor. */
 export const RapidApiInterceptorProvider: Provider = {
   provide: HTTP_INTERCEPTORS,
   useClass: RapidApiInterceptor,
